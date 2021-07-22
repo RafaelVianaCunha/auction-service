dd8670105faff427516cba6a72953dc2b07aada6
Rafael Cunha <rafaelvianacunha@outlook.com>
2021-07-21 22:50:10 -0300

    Add message to sqs

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -9,6 +9,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   if (typeof amount !== "number") {
     throw createError(400, "Amount must be a number");
@@ -24,13 +25,22 @@ async function placeBid(event, context) {
     throw createError.Forbidden("You cannot bid lower than the current highest bid");
   }
 
+  // if(auction.highestBid.bidder === email){
+  //   throw createError.Forbidden("You cannot bid on your own auctions");
+  // }
+  
+  // if(auction.highestBid.email === email){
+  //   throw createError.Forbidden("You are already the highest bidder");
+  // }
+
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: 'set highestBid.amount = :amount',
+    UpdateExpression: 'set highestBid.amount = :amount, highestBid.bidder = :bidder',
     ConditionExpression: 'attribute_exists(id)',
     ExpressionAttributeValues: {
-      ':amount': amount
+      ':amount': amount,
+      ':bidder': email
     },
     ReturnValues: 'ALL_NEW',
   };
--- src/lib/closeAuctions.js
+++ src/lib/closeAuctions.js
@@ -1,11 +1,12 @@
 import AWS from 'aws-sdk';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
 
 export async function closeAuctions(auction) {
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
-    Key : { id: auction.id },
+    Key: { id: auction.id },
     UpdateExpression: 'SET #status = :status',
     ExpressionAttributeValues: {
       ':status': 'CLOSED',
@@ -16,5 +17,39 @@ export async function closeAuctions(auction) {
   };
 
   const result = await dynamodb.update(params).promise();
-  return result;
+
+  const { title, seller, highestBid } = auction;
+  const { amount, bidder } = highestBid;
+
+  if (amount === 0) {
+    await sqs.sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: ' Your item has not been sold!',
+        recipient: seller,
+        body: `Oh no! Your item "${title}" didn't get any bids. Better Luck next time!`,
+      }),
+    }).promise();
+    return;
+  }
+
+  const notifySeller = sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      subject: ' Your item has been sold!',
+      recipient: seller,
+      body: `Wooohoo! You've sold your item to ${bidder}. The highest bid was ${amount}!`,
+    }),
+  }).promise();
+
+  const notifyBidder = sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      subject: ' You won an auction!',
+      recipient: bidder,
+      body: `Wha great item you've won! The item was ${title} and you won ${amount}!`,
+    }),
+  }).promise();
+
+  return Promise.all([notifySeller, notifyBidder]);
 }
